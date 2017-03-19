c2f757c46334542a171e381ca92772d9804f9581
Sergey <serega199505@mail.ru>
2017-03-19 20:02:10 +0400

    shop

---
+++ www/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/shop.ru/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/shop.ru/www/system/');
+define('DIR_IMAGE', 'Z:/home/shop.ru/www/image/');
+define('DIR_LANGUAGE', 'Z:/home/shop.ru/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/shop.ru/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/shop.ru/www/system/config/');
+define('DIR_CACHE', 'Z:/home/shop.ru/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/shop.ru/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/shop.ru/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/shop.ru/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/shop.ru/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'serega199505');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'bd_new_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
